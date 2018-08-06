9c90c8fa49410b590cd28835589576c7aa553b21
natejfoster <nathan.96@comcast.net>
2018-08-05 23:56:06 -0700

    removed .cache

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/nathanfoster/Documents/410-Tech/website/src/layouts/index.js"
-  import data from "/Users/nathanfoster/Documents/410-Tech/website/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
